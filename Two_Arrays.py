1bf403d40900ba9df303ec13d9bf4d1e382c836f
hebele123 <hebele@mailinaator.com>
2016-06-26 07:22:34 -0700

    Two arrays problem

--- Two_Arrays.py
+++ Two_Arrays.py
@@ -3,7 +3,6 @@ t = int(raw_input())
 for i in xrange(t):
 	n, k = map(int,raw_input().split())
 	a = sorted(map(int,raw_input().split()))
-	b = sorted([k-x for x in map(int,raw_input().split())])
-	print 'YES' if len([i for i in xrange(n) if a[i]-b[i]<0])==0 else 'NO'
-			    
+	b = sorted(map(int,raw_input().split()), reverse=True)
+	print 'YES' if min([x+y for x,y in zip(a, b)])>=k else 'NO'
 			      
